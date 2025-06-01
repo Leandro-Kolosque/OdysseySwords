5c759b5a0d075627a7abc4bd086096a9348898b1
Leandro-Kolosque <161510235+Leandro-Kolosque@users.noreply.github.com>
2025-06-01 16:00:11 -0300

    Update Description Item

--- src/main/java/com/odysseyswords/customswordmod/item/custom/ModTooltipItem.java
+++ src/main/java/com/odysseyswords/customswordmod/item/custom/ModTooltipItem.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.List;
 
 public class ModTooltipItem extends Item {
@@ -17,16 +18,27 @@ public class ModTooltipItem extends Item {
         super(properties);
     }
 
-    @Override
-    public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag flag) {
-        String key = stack.getItem().getDescriptionId() + ".tooltip";
-        String itemId = stack.getItem().getDescriptionId();
-
+@Override
+public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag flag) {
+    String itemId = stack.getItem().getDescriptionId();
+    
+    // Primeiro tenta pegar a descrição específica do item
+    String specificTooltipKey = itemId + ".tooltip";
+    if (Component.translatable(specificTooltipKey).getString().equals(specificTooltipKey)) {
+        // Se não houver descrição específica, usa a genérica do material
+        Optional<String> materialTooltipKey = TooltipStyleHelper.getMaterialTooltipKey(itemId);
+        if (materialTooltipKey.isPresent()) {
+            ChatFormatting color = TooltipStyleHelper.getColorFromId(itemId);
+            tooltip.add(Component.translatable(materialTooltipKey.get()).withStyle(color, ChatFormatting.ITALIC));
+        }
+    } else {
+        // Se houver descrição específica, usa ela
         ChatFormatting color = TooltipStyleHelper.getColorFromId(itemId);
-        tooltip.add(Component.translatable(key).withStyle(color, ChatFormatting.ITALIC));
-
-        super.appendHoverText(stack, world, tooltip, flag);
+        tooltip.add(Component.translatable(specificTooltipKey).withStyle(color, ChatFormatting.ITALIC));
     }
+
+    super.appendHoverText(stack, world, tooltip, flag);
+}
 }
 
 
--- src/main/java/com/odysseyswords/customswordmod/util/TooltipStyleHelper.java
+++ src/main/java/com/odysseyswords/customswordmod/util/TooltipStyleHelper.java
@@ -4,12 +4,16 @@ import net.minecraft.ChatFormatting;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+
 
 public class TooltipStyleHelper {
-
     private static final Map<String, ChatFormatting> STYLE_MAP = new HashMap<>();
+    private static final Map<String, String> MATERIAL_TOOLTIP_MAP = new HashMap<>();
 
     static {
+        
+        // Mapeamento de cores
         STYLE_MAP.put("blood", ChatFormatting.DARK_RED);
         STYLE_MAP.put("crystal", ChatFormatting.LIGHT_PURPLE);
         STYLE_MAP.put("cyber", ChatFormatting.RED);
@@ -25,6 +29,21 @@ public class TooltipStyleHelper {
         STYLE_MAP.put("tartarus", ChatFormatting.GOLD);
         STYLE_MAP.put("toxic", ChatFormatting.DARK_GREEN);
         STYLE_MAP.put("void", ChatFormatting.DARK_PURPLE);
+        
+        // Mapeamento de descrições por material
+        MATERIAL_TOOLTIP_MAP.put("blood", "item.odysseyswords.material_tooltip.blood");
+        MATERIAL_TOOLTIP_MAP.put("crystal", "item.odysseyswords.material_tooltip.crystal");
+        MATERIAL_TOOLTIP_MAP.put("cyber", "item.odysseyswords.material_tooltip.cyber");
+        MATERIAL_TOOLTIP_MAP.put("divine", "item.odysseyswords.material_tooltip.divine");
+        MATERIAL_TOOLTIP_MAP.put("royal", "item.odysseyswords.material_tooltip.royal");
+        MATERIAL_TOOLTIP_MAP.put("frost", "item.odysseyswords.material_tooltip.frost");
+        MATERIAL_TOOLTIP_MAP.put("sculk", "item.odysseyswords.material_tooltip.sculk");
+        MATERIAL_TOOLTIP_MAP.put("silverine", "item.odysseyswords.material_tooltip.silverine");
+        MATERIAL_TOOLTIP_MAP.put("soul", "item.odysseyswords.material_tooltip.soul");
+        MATERIAL_TOOLTIP_MAP.put("tartarus", "item.odysseyswords.material_tooltip.tartarus");
+        MATERIAL_TOOLTIP_MAP.put("storm", "item.odysseyswords.material_tooltip.storm");
+        MATERIAL_TOOLTIP_MAP.put("toxic", "item.odysseyswords.material_tooltip.toxic");
+        MATERIAL_TOOLTIP_MAP.put("void", "item.odysseyswords.material_tooltip.void");
     }
 
     public static ChatFormatting getColorFromId(String itemId) {
@@ -33,5 +52,12 @@ public class TooltipStyleHelper {
             .map(Map.Entry::getValue)
             .findFirst()
             .orElse(ChatFormatting.GRAY);
+    }
+
+    public static Optional<String> getMaterialTooltipKey(String itemId) {
+        return MATERIAL_TOOLTIP_MAP.entrySet().stream()
+            .filter(entry -> itemId.contains(entry.getKey()))
+            .map(Map.Entry::getValue)
+            .findFirst();
     }
 }
