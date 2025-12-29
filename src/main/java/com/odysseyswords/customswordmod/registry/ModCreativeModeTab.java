f9e0521e0d348551acb2e9c7b9710f4c6acddbb4
Leandro-Kolosque <161510235+Leandro-Kolosque@users.noreply.github.com>
2025-12-29 02:48:59 -0300

    Update ModCreativeModeTab.java

--- src/main/java/com/odysseyswords/customswordmod/registry/ModCreativeModeTab.java
+++ src/main/java/com/odysseyswords/customswordmod/registry/ModCreativeModeTab.java
@@ -11,15 +11,38 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModCreativeModeTab {
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, OdysseySwords.MODID);
 
-    public static final RegistryObject<CreativeModeTab> ODYSSEY_TAB = CREATIVE_MODE_TABS.register("odyssey_tab", () -> CreativeModeTab.builder()
-            .icon(() -> new ItemStack(ModItems.STEEL_SWORD.get()))
-            .title(Component.translatable("Odyssey Swords"))
-            .displayItems((itemDisplayParameters, output) -> {
-                ModItems.ITEMS.getEntries().stream().map(RegistryObject::get).forEach(output::accept);
-                output.accept(ModBlocks.MYTHIC_FORGE.get());
-            }).build());
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, OdysseySwords.MODID);
+
+    public static final RegistryObject<CreativeModeTab> ODYSSEY_TAB =
+            CREATIVE_MODE_TABS.register("odyssey", () ->
+                    CreativeModeTab.builder()
+                            .title(Component.translatable("creative_tab.odysseyswords.odyssey"))
+                            .icon(ModCreativeModeTab::makeIcon)
+                            .displayItems(ModCreativeModeTab::fillTab)
+                            .build()
+            );
+
+    private static ItemStack makeIcon() {
+        return new ItemStack(ModItems.STEEL_SWORD.get());
+    }
+
+    private static void fillTab(CreativeModeTab.ItemDisplayParameters params,
+                                CreativeModeTab.Output output) {
+
+        // Itens
+        ModItems.ITEMS.getEntries()
+                .stream()
+                .map(RegistryObject::get)
+                .forEach(output::accept);
+
+        // Blocos
+        ModBlocks.BLOCKS.getEntries()
+                .stream()
+                .map(RegistryObject::get)
+                .forEach(output::accept);
+    }
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
